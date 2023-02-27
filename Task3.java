5637662c93d52b20d5431704bb20c73591cf5d91
Sjiden <123207112+Sjiden@users.noreply.github.com>
2023-02-27 11:16:34 -0500

    Add files via upload
    Answers for Task 3

---
+++ Task3.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Luis A. Mendez
+ */
+
+/*
+int fib(int n)
+
+{
+       if (n <= 0) 
+
+            return 0;
+
+      else if (n == 1) 
+
+             return 1;
+
+       else
+
+             return fib(n – 1) + fib(n – 2);
+
+}
+
+The time complexity of this algorithm is O(2^n). The algorithm calls itself twice
+for values higher then 1. These calls each individually will call the algorithm
+recursively 2 times until the input equals 0 or 1. This means that the complexity 
+is T(n) = T(n-1) + T(n-2) + 1, the +1 being the first call to the algorithm. In 
+Big O notation, this is O(2^n)
+*/
+
+
